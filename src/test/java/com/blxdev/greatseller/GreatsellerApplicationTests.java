68bdaf4a11399206029a0416795d916f085f1bff
pcvm24531 <pcvm2453@gmail.com>
2025-04-24 16:58:43 -0400

    Add files via upload

---
+++ src/test/java/com/blxdev/greatseller/GreatsellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blxdev.greatseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatsellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
